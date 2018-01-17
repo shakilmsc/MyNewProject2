ddaad70d4efc1f3643178679fbc3662be78523a6
Shakil Ahmed <shakilmsc@gmail.com>
2018-01-17 14:30:19 -0500

    added another code

---
+++ src/mynewcode/HelloCode3.java
@@ -0,0 +1,10 @@
+package mynewcode;
+
+public class HelloCode3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Practicing writing code");
+	}
+
+}
